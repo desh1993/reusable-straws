4c332cc25da20c291e03ceaf03e36571a62a8b83
desh1993 <szemingrage@gmail.com>
2024-06-16 11:11:50 +0800

    first commit

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
-import MobileMenu from './mobile-menu'
+import Link from "next/link";
+import MobileMenu from "./mobile-menu";
 
 export default function Header() {
   return (
@@ -10,8 +10,20 @@ export default function Header() {
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link href="/" className="block" aria-label="Cruip">
-              <svg className="w-8 h-8 fill-current text-purple-600" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+              {/* <svg className="w-8 h-8 fill-current text-purple-600" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                 <path d="M31.952 14.751a260.51 260.51 0 00-4.359-4.407C23.932 6.734 20.16 3.182 16.171 0c1.634.017 3.21.28 4.692.751 3.487 3.114 6.846 6.398 10.163 9.737.493 1.346.811 2.776.926 4.262zm-1.388 7.883c-2.496-2.597-5.051-5.12-7.737-7.471-3.706-3.246-10.693-9.81-15.736-7.418-4.552 2.158-4.717 10.543-4.96 16.238A15.926 15.926 0 010 16C0 9.799 3.528 4.421 8.686 1.766c1.82.593 3.593 1.675 5.038 2.587 6.569 4.14 12.29 9.71 17.792 15.57-.237.94-.557 1.846-.952 2.711zm-4.505 5.81a56.161 56.161 0 00-1.007-.823c-2.574-2.054-6.087-4.805-9.394-4.044-3.022.695-4.264 4.267-4.97 7.52a15.945 15.945 0 01-3.665-1.85c.366-3.242.89-6.675 2.405-9.364 2.315-4.107 6.287-3.072 9.613-1.132 3.36 1.96 6.417 4.572 9.313 7.417a16.097 16.097 0 01-2.295 2.275z" />
+              </svg> */}
+              <svg
+                width="64px"
+                height="64px"
+                viewBox="0 0 64 64"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M52.96 11.04a3 3 0 0 0-4.24 0L36.12 23.64l-2.83-2.83L18.96 35.14l-4.94-4.94a3 3 0 0 0-4.24 0l-4 4a3 3 0 0 0 0 4.24l4.94 4.94L2 57.46a3 3 0 1 0 4.24 4.24l11.72-11.72 4.94 4.94a3 3 0 0 0 4.24 0l4-4a3 3 0 0 0 0-4.24l-4.94-4.94 14.34-14.34 2.83 2.83 12.6-12.6a3 3 0 0 0 0-4.24l-4-4zm-32 36.4a1 1 0 0 1-1.41 0l-6-6a1 1 0 0 1 0-1.41l2-2a1 1 0 0 1 1.41 0l6 6a1 1 0 0 1 0 1.41l-2 2z"
+                  fill="#ffffff"
+                />
               </svg>
             </Link>
           </div>
@@ -29,7 +41,10 @@ export default function Header() {
                 </Link>
               </li>
               <li>
-                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                <Link
+                  href="/signup"
+                  className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3"
+                >
                   Sign up
                 </Link>
               </li>
@@ -37,9 +52,8 @@ export default function Header() {
           </nav>
 
           <MobileMenu />
-
         </div>
       </div>
     </header>
-  )
+  );
 }
